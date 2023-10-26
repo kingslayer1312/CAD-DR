30b72700a80f9011fe6e2c92a5890af0a7092414
Kingslayer1312 <hrishabari@gmail.com>
2023-10-26 14:52:23 +0530

    completed v3, brought down loss to 0.0176

--- ConversionUtils.py
+++ ConversionUtils.py
@@ -13,18 +13,18 @@ class ConversionUtils:
                 file_list.append(filename)
         return file_list
     
-    def stl_to_ply(path, x):
+    def stl_to_ply(path, number_of_points):
         imported_file_name = Path(path).stem
         mesh = o3d.io.read_triangle_mesh(path)
-        pcd = mesh.sample_points_uniformly(number_of_points=x)
+        pcd = mesh.sample_points_uniformly(number_of_points=number_of_points)
         exported_file_name = "abc-dataset-ply/" + imported_file_name + ".ply"
         o3d.io.write_point_cloud(exported_file_name, pcd, write_ascii=True, compressed=False, print_progress=False)
     
-    def convert_to_binvox(path):
+    def convert_to_binvox(path, dim):
         point_cloud = np.loadtxt(path, skiprows=12)[:, 0:3]
         df = pd.DataFrame(data=point_cloud, columns=['x','y','z'])
         cloud = PyntCloud(df)
-        voxelgrid_id = cloud.add_structure("voxelgrid", n_x=64, n_y=64, n_z=64)
+        voxelgrid_id = cloud.add_structure("voxelgrid", n_x=dim, n_y=dim, n_z=dim)
         voxelgrid = cloud.structures[voxelgrid_id]
         Binary_voxel_array = voxelgrid.get_feature_vector(mode="binary")
         return Binary_voxel_array
